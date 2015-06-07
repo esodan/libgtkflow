13655064fd996e2418f48419576e36f3008beed1
grindhold <grindhold@gmx.net>
2015-06-07 14:41:38 +0200

    added new example
    this example shows how to use libgtkflow in a scrolled
    window and it shows how to create nodes with a dynamic
    amount of docks.

--- examples/scrolling_multidock.py
+++ examples/scrolling_multidock.py
@@ -5,6 +5,60 @@ from gi.repository import GtkFlow
 
 import sys
 
+class AddNode(GtkFlow.Node):
+    def add_summand(self, widget=None, data=None):
+        summand_a = GtkFlow.Sink.new(float(0))
+        summand_a.set_label("operand %i"%(len(self.summands),))
+        self.add_sink(summand_a)
+        summand_a.connect("changed", self.do_calculations)
+        self.summands.append(summand_a)
+ 
+    def remove_summand(self, widget=None, data=None):
+        summand = self.summands[len(self.summands)-1]
+        summand.unset_source()
+        self.remove_sink(summand)
+        self.summands.remove(summand)
+        summand.destroy()
+        self.do_calculations(None)
+        
+       
+    def __init__(self):
+        GtkFlow.Node.__init__(self)
+
+        self.summands = []
+    
+        self.result = GtkFlow.Source.new(float(0))
+        self.result.set_label("result")
+        self.add_source(self.result)
+
+        self.add_button = Gtk.Button.new_with_mnemonic("Add")
+        self.remove_button = Gtk.Button.new_with_mnemonic("Rem")
+        self.btnbox = Gtk.Box.new(Gtk.Orientation.HORIZONTAL,0)
+        self.btnbox.add(self.add_button)
+        self.btnbox.add(self.remove_button)
+        self.add_button.connect("clicked", self.add_summand)
+        self.remove_button.connect("clicked", self.remove_summand)
+        self.add(self.btnbox)
+        self.show_all()
+
+
+        self.set_title("Operation")
+    
+        self.set_border_width(10)
+
+    def do_calculations(self, dock, val=None):
+        print ("ohai")
+        res = 0
+        for summand in self.summands:
+            try:
+                val = summand.get_value()
+                res += val
+            except:
+                self.result.invalidate()
+                return
+    
+        self.result.set_value(res)
+
 class OperationNode(GtkFlow.Node):
     def __init__(self):
         GtkFlow.Node.__init__(self)
@@ -138,7 +192,7 @@ class Calculator(object):
         Gtk.main()
 
     def do_create_addnode(self, widget=None, data=None):
-        self.nv.add(OperationNode())
+        self.nv.add(AddNode())
     def do_create_numbernode(self, widget=None, data=None):
         self.nv.add(NumberNode())
     def do_create_printnode(self, widget=None, data=None):
